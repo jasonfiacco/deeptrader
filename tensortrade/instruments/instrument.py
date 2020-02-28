63ab0fd13c195604c237c216de53d63c08a68e52
Jason Fiacco <jason.fiacco@yale.edu>
2020-02-28 14:21:09 -0500

    Handled merge conflicts from the new rebasing

--- tensortrade/instruments/instrument.py
+++ tensortrade/instruments/instrument.py
@@ -94,7 +94,33 @@ XAG = Instrument('XAG', 2, 'Silver futures')
 
 # Stocks
 
-AAPL = Instrument('AAPL', 2, 'Apple stock')
-MSFT = Instrument('MSFT', 2, 'Microsoft stock')
-TSLA = Instrument('TSLA', 2, 'Tesla stock')
-AMZN = Instrument('AMZN', 2, 'Amazon stock')
+# Hard Currency
+GOLD = Instrument('XAU', 1, 'Gold')
+
+
+#PredictIt
+TRUMP_PRESIDENT = Instrument('TRUMP_PRESIDENT', 2, 'Will Donald Trump be president at year-end 2018?')
+WARREN = Instrument('WARREN', 2, 'Will Elizabeth Warren be re-elected to the U.S. Senate in Massachusetts in 2018?')
+CRUZ = Instrument('CRUZ', 2, 'Will Ted Cruz be re-elected to the U.S. Senate in Texas in 2018?')
+MANCHIN = Instrument('MANCHIN', 2, 'Will Joe Manchin be re-elected to the U.S. Senate in West Virginia in 2018?')
+SANDERS = Instrument('SANDERS', 2, 'Will Bernie Sanders be re-elected to the U.S. Senate in Vermont in 2018?')
+NELSON = Instrument('NELSON', 2, 'Will Bill Nelson be re-elected to the U.S. Senate in Florida in 2018?')
+DONNELLY = Instrument('DONNELLY', 2, 'Will Joe Donnelly be re-elected to the U.S. Senate in Indiana in 2018?')
+TRUMP_IMPEACH = Instrument('TRUMP_IMPEACH', 2, 'Will Donald Trump be impeached by year-end 2018?')
+FL_SENATE = Instrument('FL_SENATE', 2, 'Who will win the 2018 Florida Republican Senate primary?')
+MANAFORT = Instrument('MANAFORT', 2, 'Will Trump pardon Manafort by year-end 2018?')
+CALEXIT = Instrument('CALEXIT', 2, 'Will "Calexit" initiative qualify for the 2018 ballot in California?')
+TRUMP_APPROVAL = Instrument('TRUMP_APPROVAL', 2, 'Will Trump\'s Gallup approval be 50% or higher at any point before year-end 2018?')
+RYAN = Instrument('RYAN', 2, 'Will Paul Ryan be re-elected to Congress in 2018?')
+PELOSI = Instrument('PELOSI', 2, 'Will Nancy Pelosi be re-elected to Congress in 2018?')
+HURD = Instrument('HURD', 2, 'Will Will Hurd be re-elected to Congress in 2018?')
+TESTER = Instrument('TESTER', 2, 'Will Jon Tester be re-elected to the U.S. Senate in Montana in 2018?')
+HEITKAMP = Instrument('HEITKAMP', 2, 'Will Heidi Heitkamp be re-elected to the U.S. Senate in North Dakota in 2018?')
+DENHAM = Instrument('DENHAM', 2, 'Will Jeff Denham be re-elected to Congress in 2018?')
+BALDWIN = Instrument('BALDWIN', 2, 'Will Tammy Baldwin be re-elected to the U.S. Senate in Wisconsin in 2018?')
+MCCASKILL = Instrument('MCCASKILL', 2, 'Will Claire McCaskill be re-elected to the U.S. Senate in Missouri in 2018?')
+KNIGHT = Instrument('KNIGHT', 2, 'Will Steve Knight be re-elected to Congress in 2018?')
+BROWN = Instrument('BROWN', 2, 'Will Sherrod Brown be re-elected to the U.S. Senate in Ohio in 2018?')
+STABENOW = Instrument('STABENOW', 2, 'Will Debbie Stabenow be re-elected to the U.S. Senate in Michigan in 2018?')
+HEINRICH = Instrument('HEINRICH', 2, 'Will Martin Heinrich be re-elected to the U.S. Senate in New Mexico in 2018?')
+MENENDEZ = Instrument('MENENDEZ', 2, 'Will Bob Menendez be re-elected to the U.S. Senate in New Jersey in 2018?')
